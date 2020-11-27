f05935b106b00e1d750fb0a1732915b8af129d13
Pashko Ostap <pashko.ostap@gmail.com>
2020-11-27 12:15:37 +0200

    updated client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ticketing-ingress-srv',
       headers: req.headers,
     });
   }

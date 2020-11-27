d493f7527506bdca8ee3fd85390f6c7accf0da73
Pashko Ostap <pashko.ostap@gmail.com>
2020-11-27 12:31:17 +0200

    updated client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
       // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL: 'http://ticketing.pashkoostap.cloud.okteto.net',
+      baseURL: '/',
       headers: req.headers,
     });
   }
